7ea266c02ca6c057e6a9b0477f9bb4f5adcc547e
VesterOn <kiril.stovba1501@gmail.com>
2022-01-18 14:31:22 +0200

    Create Testing_Program

---
+++ Testing_Program/include.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <conio.h>
+#include <fstream>
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <direct.h>
+#include "nlohmann/json.hpp"
+#include <vector>
+using namespace nlohmann;
+using namespace std;
